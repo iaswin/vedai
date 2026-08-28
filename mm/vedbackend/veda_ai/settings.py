c530dfd0a4f98ef7f90790c72a34bf2e74631f10
Aswin <117162456+iaswin@users.noreply.github.com>
2026-08-28 16:18:27 +0530

    Add CORS and CSRF trusted origins for deploymentff

--- mm/vedbackend/veda_ai/settings.py
+++ mm/vedbackend/veda_ai/settings.py
@@ -154,6 +154,7 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://vedfrontend-3.onrender.com"
 
    
 ]
@@ -161,6 +162,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://vedfrontend-3.onrender.com"
  
 
     
